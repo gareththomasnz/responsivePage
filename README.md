1036f8d9c736f4cf5c9f8376fa1f9d9e7ee306ab
gareththomasnz <gareththomasnz@yahoo.com>
2016-07-03 08:13:44 +1200

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# responsiveHTML5
+Simple HTML5 Boilerplate exercise
+
+
+## Built With
+
+Komodo - hand coding exercise
+
+
+## Authors
+
+Gareth Thomas
+
+## License
+
+This is coded from a commercial course - not public domain
+
+## Acknowledgments
+
+Pluralsight Get Started With HTML5 Boilerplate
---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+(function($){
+	// Set custom style, close if clicked, change title type and overlay color
+	$(".fancybox").fancybox({
+		wrapCSS    : 'fancybox-custom',
+		closeClick : true,
+
+		openEffect : 'none',
+
+		helpers : {
+			title : {
+				type : 'inside'
+			},
+			overlay : {
+				css : {
+					'background' : 'rgba(238,238,238,0.85)'
+				}
+			}
+		}
+	});
+})(jQuery);
